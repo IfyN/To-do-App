6c597ccdf80064e309329cf98956ec00fb5ec38d
Ifeoma Nwosu <31158530+IfyN@users.noreply.github.com>
2019-03-27 00:36:09 +0100

    Add files via upload
    The main folder of this project is titled 'vue-skills' in it contains all the files created when building a Vue Project- from the node modules to the README. But I could not upload it all because of the size of the files, just the folders which contain my written codes, that is the 'public' and 'src' folders.

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+  <h1>Hello {{their_name}}, About page </h1>
+  <p> This is a To do App</p>
+  </div>
+  </template>
+
+<script>
+export default {
+  name: 'skills',
+  data () 
+  {
+  return {
+    their_name: this.$route.params.name
+
+}
+  
+  }
+}
+
+</script>
